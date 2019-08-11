122c0dbd4dee5057e6d1733f87bb035b40a8e43d
yotkoKanchev <yotko.kanchev@gmail.com>
2019-08-11 15:12:39 +0300

    Added washing icons

--- gulpfile.js
+++ gulpfile.js
@@ -7,29 +7,28 @@ let gulp = require("gulp"),
 	sass = require('gulp-sass'),
 	cp = require("child_process");
 
-gulp.task("css", function() {
-	return gulp.src( '_assets/css/**/*.css' )
+gulp.task("css", function () {
+	return gulp.src('_assets/css/**/*.css')
 		// .pipe( sass().on('error', sass.logError) )
-		.pipe( autoprefixer() )
-		.pipe( gulp.dest( './docs/css/' ) )
-		.pipe( browserSync.stream({ match: '**/*.css' }) )
-	;
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('./docs/css/'))
+		.pipe(browserSync.stream({ match: '**/*.css' }))
 });
 
 // Jekyll
-gulp.task("jekyll", function() {
+gulp.task("jekyll", function () {
 	return cp.spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit", shell: true });
 });
 
-gulp.task("watch", function() {
+gulp.task("watch", function () {
 
 	browserSync.init({
 		server: {
-            baseDir: "./docs/"
+			baseDir: "./docs/"
 		}
 	});
 
-	gulp.watch( '_assets/css/**/*.css', gulp.series('css') );
+	gulp.watch('_assets/css/**/*.css', gulp.series('css'));
 
 	gulp.watch(
 		[
@@ -38,10 +37,10 @@ gulp.task("watch", function() {
 			"./_layouts/*.html",
 			"./_posts/**/*.*"
 		]
-	).on('change', gulp.series('jekyll', 'css') );
+	).on('change', gulp.series('jekyll', 'css'));
 
-	gulp.watch( 'docs/**/*.html' ).on('change', browserSync.reload );
-	gulp.watch( 'docs/**/*.js' ).on('change', browserSync.reload );
+	gulp.watch('docs/**/*.html').on('change', browserSync.reload);
+	gulp.watch('docs/**/*.js').on('change', browserSync.reload);
 
 });
 
