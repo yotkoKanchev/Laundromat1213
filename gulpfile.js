865c003c8ca7ea80c52fd15bd3b8558d8cdf9321
yotkoKanchev <yotko.kanchev@gmail.com>
2019-08-15 00:03:46 +0300

    Change css to csss, formated some files

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,8 @@ let gulp = require("gulp"),
 	cp = require("child_process");
 
 gulp.task("css", function () {
-	return gulp.src('_assets/css/**/*.css')
-		// .pipe( sass().on('error', sass.logError) )
+	return gulp.src('_assets/scss/**/*.scss')
+		.pipe( sass().on('error', sass.logError) )
 		.pipe(autoprefixer())
 		.pipe(gulp.dest('./docs/css/'))
 		.pipe(browserSync.stream({ match: '**/*.css' }))
@@ -34,7 +34,7 @@ gulp.task("watch", function () {
 		}
 	});
 
-	gulp.watch('_assets/css/**/*.css', gulp.series('css'));
+	gulp.watch('_assets/scss/**/*.scss', gulp.series('css'));
 
 	gulp.watch(
 		[
